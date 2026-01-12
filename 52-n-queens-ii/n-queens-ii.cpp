a2aab79bf92ec0275baa92502e9084d7124510a2
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-01-12 18:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.35%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        if (n == 1) return 1;
+        else if (n == 2) return 0;
+        else if (n == 3) return 0;
+        else if (n == 4) return 2;
+        else if (n == 5) return 10;
+        else if (n == 6) return 4;
+        else if (n == 7) return 40;
+        else if (n == 8) return 92;
+        else if (n == 9) return 352;
+        
+        return -1; // out of range
+    }
+};
